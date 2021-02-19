e0a0daf94fe4871d1690f4b6aba112dce4d7e3d5
mattruma <mattruma@gmail.com>
2021-02-19 13:11:00 -0500

    auth

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,8 +1,9 @@
+using AzureStaticWebApps.Blazor.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace BlazorApp.Client
 {
@@ -11,10 +12,12 @@ namespace BlazorApp.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
             builder.RootComponents.Add<App>("app");
 
             var baseAddress = builder.Configuration["BaseAddress"] ?? builder.HostEnvironment.BaseAddress;
-            builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(baseAddress) });
+
+            builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(baseAddress) }).AddStaticWebAppsAuthentication(); ;
 
             await builder.Build().RunAsync();
         }
